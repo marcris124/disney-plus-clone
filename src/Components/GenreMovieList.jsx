5f2bdd3b88edea9c9d9bdceebd90e2be0a8d9669
Martin Santiago Aguirre <martinavendano095@gmail.com>
2024-01-09 14:16:51 -0500

    add genres

--- src/Components/GenreMovieList.jsx
+++ src/Components/GenreMovieList.jsx
@@ -5,7 +5,7 @@ import MovieList from './MovieList'
 const GenreMovieList = () => {
   return (
     <div>{
-        GenresList.genere.map((item,index)=>index<=4&&(
+        GenresList.genere.map((item,index)=>index<=10&&(
             <div key={item.id} className='p-0 md:p-3 px-8 md:px-16'>
                 <h2 className='text-[20px] text-white font-bold'> {item.name} </h2>
                 <MovieList genreId={item.id} index_={index}/>
